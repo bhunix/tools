05132d36335423a3e2d3f71469b36a89c0de9e34
yangzhijie <yangzhijie@vimtag.it>
2025-03-20 10:04:59 +0800

    delete env.sh

--- env.sh
+++
@@ -1,5 +0,0 @@
-grep -o 'csrftoken=[^;]*' cookie | sed s/csrftoken/export\ LEETCODE_CSRFTOKEN/ >> env
-grep -o 'LEETCODE_SESSION=[^;]*' cookie | sed s/LEETCODE_SESSION/export\ LEETCODE_SESSION/ >> env
-grep -o 'cf_clearance=[^;]*' cookie | sed s/cf_clearance/export\ cf_clearance/ >> env
-
-
